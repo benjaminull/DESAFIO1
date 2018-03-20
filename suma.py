c02eefbf7783c751c51e9157df18024a8baa769d
nicol ull <benjamin.ull.m@gmail.com>
2018-03-20 17:48:28 -0300

    primer commit

---
+++ suma.py
@@ -0,0 +1,13 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> def suma(x,y)
+SyntaxError: invalid syntax
+>>> suma(x,y)
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    suma(x,y)
+NameError: name 'suma' is not defined
+>>> def suma (x,y):
+	return x+y
+
+>>> 
